93400dfd64e5f9f0e2f56fb2891134b29f422e73
choi <wjfekr1@naver.com>
2023-01-16 23:05:05 +0900

    로그인 만드는 중, 결과보기 오류발생

--- app.py
+++ app.py
@@ -1,12 +1,78 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
 app = Flask(__name__)
 
+from pymongo import MongoClient
+from datetime import datetime, timedelta
+import jwt
+import hashlib
+
+from pymongo import MongoClient
+import certifi
+
+SECRET_KEY = 'SPARTA'
+
+ca = certifi.where();
+
+client = MongoClient('mongodb+srv://seungho:19960724@cluster0.y2z4pif.mongodb.net/?retryWrites=true&w=majority',
+    tlsCAFile=ca)
+db = client.dbsparta
+
 @app.route('/')
-def hello_world():
-    return render_template('emoji.html')
+def home():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"id": payload['id']})
+        return render_template('index.html', user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for('login', msg='로그인 시간이 만료되었습니다.'))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for('login', msg='로그인 정보가 존재하지 않습니다.'))
+
+
+
+@app.route("/sign_up", methods=["POST"])
+def login_done():
+    email_receive = request.form['email_give']
+    pw_receive = request.form['pw_give']
+
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "email":email_receive, "password":pw_hash
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+
+
+
+# 로그인
+
+@app.route('/sign_in', methods=['POST'])
+def api_login():
+    email_receive = request.form['email_give']
+    pw_receive = request.form['pw_give']
+
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    result = db.users.find_one({'email': email_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+            'id': email_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=5)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        # token을 줍니다.
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
 
 
 
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5001, debug=True)
+    app.run('0.0.0.0', port=5100, debug=True)
--- static/index.js
+++ static/index.js
@@ -8,13 +8,15 @@ const J = document.getElementById("J");
 const P = document.getElementById("P");
 const startButton = document.getElementById("start-button");
 const alphabets = document.querySelectorAll(".alphabet");
+const loginArea = document.getElementById("login-area");
+const signUpButton = document.getElementById("sign_up_button");
+const signInButton = document.getElementById("sign_in_button");
 
 const changeCheck = (a) => {
   if (a.classList.contains("btn-info")) {
     return;
   }
   let b = a.innerHTML;
-  console.log(b);
   if (b == "E" || b == "I") {
     E.classList.toggle("btn-info");
     I.classList.toggle("btn-info");
@@ -45,3 +47,43 @@ const getStart = () => {
 };
 
 startButton.addEventListener("click", getStart);
+
+function sign_up() {
+  let email = document.getElementById("floatingInput").value;
+  let password = document.getElementById("floatingPassword").value;
+
+  $.ajax({
+    type: "POST",
+    url: "/sign_up",
+    data: { email_give: email, pw_give: password },
+    success: function (response) {
+      if (response["result"] == "success") {
+        alert("회원가입이 완료되었습니다.");
+      } else {
+        alert(response["msg"]);
+      }
+    },
+  });
+}
+
+const sign_in = () => {
+  let email = document.getElementById("floatingInput").value;
+  let password = document.getElementById("floatingPassword").value;
+
+  $.ajax({
+    type: "POST",
+    url: "/sign_in",
+    data: { email_give: email, pw_give: password },
+    success: function (response) {
+      if (response["result"] == "success") {
+        $.cookie("mytoken", response["token"], { path: "/" });
+        alert("로그인 완료!");
+      } else {
+        alert(response["msg"]);
+      }
+    },
+  });
+};
+
+// signUpButton.addEventListener("click", sign_up);
+signInButton.addEventListener("click", sign_in);
